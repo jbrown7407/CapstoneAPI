749030caa9b39aa5be9daeae9df9d0111713e13b
jbrown7407 <jbrown7407@gmail.com>
2020-12-09 14:01:35 -0500

    db adjustment

--- seed.sql
+++ seed.sql
@@ -1,2 +1,2 @@
 DELETE FROM meals;
-INSERT INTO meals (meal, restlink, pic) VALUES (''v','restaurant','pic');
+INSERT INTO meal (meal, restlink, pic) VALUES ('Burger','Marvolo Burgers','https://pyxis.nymag.com/v1/imgs/ea4/206/cb647afc35cb05bfb4be4a768a56271c76-19-shake-shack-lockhart-link-burger.rsquare.w700.jpg');
